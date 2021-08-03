9c2b7c1ff524dcce46b0e299490f2a00da1d2b46
nonseodion <nonseifebhorgmail.com>
2021-08-03 14:08:46 +0100

    added upgrade script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,8 +12,12 @@ async function main() {
   const farm = await upgrades.deployProxy(Farm, [pred.address, predPerBlock, 0, wallet.address], {kind: "uups"})
   await wallet.setMasterPred(farm.address);
 
-  console.log(`Farm deployed to:${farm.address}, wallet deployed to:${wallet.address}`);
-}
+  console.log(`Farm deployed to:${farm.address}, wallet deployed to:${wallet.address}`,
+  `implementation deployed to:${await ethers.provider.getStorageAt(
+    farm.address,
+    "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+    )}`);
+};
 
 main()
   .then(() => process.exit(0))
